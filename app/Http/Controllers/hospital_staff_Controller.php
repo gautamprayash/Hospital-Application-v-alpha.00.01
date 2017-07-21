886fc6f33fe307bf5db95526a49fd945c098e73c
gautamprayash <sudhanshuprayash@gmail.com>
2017-07-21 11:27:21 +0545

    Hospital Admin and Hospital Staff

---
+++ app/Http/Controllers/hospital_staff_Controller.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Hospital_staff;
+use Illuminate\Http\Request;
+
+class hospital_staff_Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'bail|required|',
+            'email'=>'bail|required|',
+            'contact'=>'bail|required|',
+            'password'=>'bail|required|',
+        ]);
+        $hs=new Hospital_staff;
+        $hs->name=$request->name;
+        $hs->contact=$request->contact;
+        $hs->email=$request->email;
+        $hs->password=bcrypt($request->password);
+        if ($hs->save()) {
+            return "successfully created New Hospital Staff";
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $hs=new hospital_staff;
+        return $hs::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/hospital_staff.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class hospital_staff extends Model
+{
+    protected $table='hospital_staff';
+}
